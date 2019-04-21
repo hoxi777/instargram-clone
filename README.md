c2ff466c3d99b9ba989e6f4e0b25f10fea19033d
hoxi777 <jtho1015@gmail.com>
2019-04-21 16:52:47 +0900

    Back-end createAccount

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # instargram-clone
+
 instargram clone with React + prisma + express and React-Native
+
+## User Stroies
+
+- [x] login in
+- [ ] See the feed
+- [ ] Like / Unlike a photo
+- [ ] Comment on a photo
+- [ ] Search by User
+- [ ] Search by Location
+- [ ] follow / following
+- [ ] See User profile
+- [ ] See the full photo
+- [ ] Edit my profile
+- [ ] Create account
+- [ ] Upload a photo
+- [ ] Edit photos (Delete)
--- src/schema.js
+++ src/schema.js
@@ -3,8 +3,8 @@ import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas"
 import path from "path"
 
 
-const allTypes = fileLoader(path.join(__dirname, "/api/**/*.graphql"));
-const allResolvers = fileLoader(path.join(__dirname, "/api/**/*.js"));
+const allTypes = fileLoader(path.join(__dirname, "/api/**/**/*.graphql"));
+const allResolvers = fileLoader(path.join(__dirname, "/api/**/**/*.js"));
 
 const schema = makeExecutableSchema({
     typeDefs: mergeTypes(allTypes),
